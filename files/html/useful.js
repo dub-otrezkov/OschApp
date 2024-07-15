f84a76c45e7aed14ed362c68e6c2d71df13da973
dantoper556 <120361032+dantoper556@users.noreply.github.com>
2024-07-15 11:25:16 +0300

    changes

---
+++ files/html/useful.js
@@ -0,0 +1,14 @@
+"use strict"
+
+async function combinePromises(...funcs) {
+    const results = await Promise.all(funcs.map(func => func()));
+    console.log(results)
+
+    var vls = {}
+    results.forEach((result, index) => {
+        vls[funcs[index].name] = result;
+    });
+
+    console.log(vls);
+    return vls;
+}
